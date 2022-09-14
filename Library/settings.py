e927e20b734e95b4d49a5e401518dac2f7a7c546
Motilal Chavhan <motilalvijaychavhan@gmail.com>
2022-09-14 13:38:32 +0530

    added secret key to .evn

--- Library/settings.py
+++ Library/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
+import os, dotenv
+dotenv.load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#ba)r69(8=lfrn%x*^&&qmoq&_5xr4*@+7@wu#!g+)hhr8bshs'
+SECRET_KEY = os.getenv('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
